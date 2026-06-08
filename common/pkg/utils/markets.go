adf4f070aa3e2ef043c39b4241a947035bed2516
Alexander <alex@stable.io>
2026-06-08 08:59:05 -0400

    Move markets utils from api to common

--- common/pkg/utils/markets.go
+++ common/pkg/utils/markets.go
@@ -5,12 +5,18 @@ import (
 	"strings"
 )
 
+const MARKET_SEPARATOR = "-"
+
 var ErrInvalidMarketRef = errors.New("invalid market reference, expected BASE/QUOTE")
 
 func SplitMarketRef(marketRef string) (baseSymbol, quoteSymbol string, err error) {
-	parts := strings.SplitN(marketRef, "-", 2)
+	parts := strings.SplitN(marketRef, MARKET_SEPARATOR, 2)
 	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
 		return "", "", ErrInvalidMarketRef
 	}
 	return parts[0], parts[1], nil
+}
+
+func MergeMarketRef(baseSymbol, quoteSymbol string) string {
+	return baseSymbol + MARKET_SEPARATOR + quoteSymbol
 }
