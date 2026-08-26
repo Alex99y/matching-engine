4d3abe2f034e0cae748e5c3fb4c83d5e64a0692b
Alex Yammine <Alexyammine94@gmail.com>
2026-08-26 10:13:16 -0400

    Add db rollback scripts (#8)

--- db/cmd/main.go
+++ db/cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/alex99y/matching-engine/common/pkg/config"
 	"github.com/alex99y/matching-engine/common/pkg/logger"
@@ -19,12 +22,39 @@ func main() {
 		panic(err)
 	}
 
-	logger.Info("Migrating database")
 	m, err := migrate.New("file://../migrations", postgresURL)
 	if err != nil {
 		panic(err)
 	}
+
+	if len(os.Args) > 1 && os.Args[1] == "down" {
+		steps, err := parseSteps(os.Args[2:])
+		if err != nil {
+			panic(err)
+		}
+		logger.Info("Rolling back database")
+		if err := m.Steps(-steps); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+			panic(err)
+		}
+		return
+	}
+
+	logger.Info("Migrating database")
 	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
 		panic(err)
 	}
+}
+
+func parseSteps(args []string) (int, error) {
+	if len(args) == 0 {
+		return 1, nil
+	}
+	steps, err := strconv.Atoi(args[0])
+	if err != nil {
+		return 0, fmt.Errorf("invalid steps argument %q: %w", args[0], err)
+	}
+	if steps <= 0 {
+		return 0, fmt.Errorf("steps must be a positive integer, got %d", steps)
+	}
+	return steps, nil
 }
---
+++ db/migrations/000001_init.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE matches;
+DROP TABLE cancelled_orders;
+DROP TABLE open_orders;
+DROP TABLE orders;
+DROP TABLE markets;
+DROP TABLE user_balances;
+DROP TABLE instruments;
+DROP TABLE users;
